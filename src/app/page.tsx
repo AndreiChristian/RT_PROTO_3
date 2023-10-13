81285eafbc114b455177dbe888de44c42f7eb66e
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-10-13 17:00:08 +0300

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import prisma from "@/prisma"
 
-export default function Home() {
+export default async function Home() {
+
+  const count = await prisma.user.count()
+  if(count === 0){
+    console.log("No users yet")
+  }
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <h2>User count: {count}</h2>
+
     </main>
   )
 }
